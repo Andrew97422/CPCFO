d6fd9003bf68b210809a2fb40144b19724604ff6
Andrew <nosoff.4ndr@yandex.ru>
2024-04-27 10:08:34 +0300

    update main

--- back/main.py
+++ back/main.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
-from parser import parse_text
+from tika import parser  # pip install tika
+from parser import ParseHH
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -8,8 +10,51 @@ CORS(app)
 
 @app.route('/')
 def index():
-    # parse_text("https://gb.ru/courses/all")
-    return 'Hello World!'
+    return 'index'
 
 
-app.run(debug=True)
+def predict(vacancy_text):
+    return 'vacancy_text'
+
+
+@app.route('/predict', methods=['POST'])
+def predict_text():
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json':
+        response = request.json
+        prediction = predict(response['vacancy_text'])
+        return prediction
+
+
+@app.route('/predict_pdf', methods=['POST'])
+def predict_pdf():
+    file = request.files['file']
+    file.save(file.filename)
+
+    if file:
+        parsed = parser.from_file(file.filename)
+        prediction = predict(parsed['content'].strip())
+        os.remove(file.filename)
+        return prediction
+    else:
+        return "{'error': 'File not found'}"
+
+
+@app.route('/predict_url', methods=['POST'])
+def predict_url():
+    url = request.json['url']
+    parser_hh = ParseHH(url)
+
+    print(parser_hh.title())
+    print(parser_hh.description())
+    prediction = predict(parser_hh.description())
+    return prediction
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return 'Page not found', 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
